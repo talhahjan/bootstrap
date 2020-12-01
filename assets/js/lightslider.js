22d409cb6a4feac0117d055bbaded828c27b3884
Muhammad Khalid <talhah.jan@gmail.com>
2020-12-01 21:24:40 +0500

    some improvment

--- assets/js/lightslider.js
+++ assets/js/lightslider.js
@@ -182,7 +182,7 @@
             },
             controls: function () {
                 if (settings.controls) {
-                    $el.after('<div class="lSAction"><a class="lSPrev">' + settings.prevHtml + '</a><a class="lSNext">' + settings.nextHtml + '</a></div>');
+                    $el.before('<div class="lSAction"><a class="lSPrev">' + settings.prevHtml + '</a><a class="lSNext">' + settings.nextHtml + '</a></div>');
                     if (!settings.autoWidth) {
                         if (length <= settings.item) {
                             $slide.find('.lSAction').hide();
